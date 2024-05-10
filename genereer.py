bd23d9e5922723d9016cde07eb4feb34e16319a2
Ian Swart <ian.swart@redpandasoftware.co.za>
2024-05-10 21:24:01 +0200

    Add files to git repo

---
+++ genereer.py
@@ -0,0 +1,99 @@
+import random
+from datetime import datetime, timedelta
+from helper import output_csv, output_pdf, read_diakens
+
+
+def generate_schedule(months=6):
+    """
+    Generate a shuffled schedule for diakens for a specified number of months. Outputs the schedule to a CSV file.
+
+    Args:
+        months (int): The number of months for which to generate the schedule. Default is 6.
+    """
+
+    names = read_diakens()
+    random.shuffle(names)
+    print(names)
+
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    first_day = datetime(current_year, current_month, 1)
+    first_sunday = first_day + timedelta(days=(6 - first_day.weekday() + 7) % 7)
+
+    sundays = []
+    for week in range(months*4):
+        sunday = first_sunday + timedelta(weeks=week)
+
+        names_starting_index = week * 4
+
+        sunday_diakens = []
+        for i in range(4):
+            diaken_index = (names_starting_index + i) % len(names)
+            diaken = names[diaken_index].strip()
+            sunday_diakens.append(diaken)
+
+
+        random.shuffle(sunday_diakens)
+
+        sunday_dict = {
+            'sondag_datum': sunday.strftime('%Y-%m-%d'),
+            'diaken_1': sunday_diakens[0],
+            'diaken_2': sunday_diakens[1],
+            'diaken_3': sunday_diakens[2],
+            'diaken_4': sunday_diakens[3],
+        }
+
+        sundays.append(sunday_dict)
+
+    # Save the list of dicts to a CSV file
+    last_sunday = first_sunday + timedelta(weeks=months*4)
+    
+    output_csv_file_name = f"diakens_diensbeurte_{first_sunday.strftime('%Y-%m-%d')}_{last_sunday.strftime('%Y-%m-%d')}.csv"
+    output_csv(sundays, output_csv_file_name)
+
+    output_pdf_file_name = f"diakens_diensbeurte_{first_sunday.strftime('%Y-%m-%d')}_{last_sunday.strftime('%Y-%m-%d')}.pdf"
+    output_pdf(sundays, output_pdf_file_name)
+
+
+def main():
+    while True:
+        print("Welkom! Hierdie is 'n hulpmiddel om diaken diensbeurte uit te werk. Kies asseblief 'n opsie (1-3)")
+        print("1. (default) Die diakenlys word geshuffel en ingedeel per sondag. Ingedeelde diakens word weer geshuffel.")
+        print("2. Dieselfde as opsie 1, maar die hele diakenlys word elke silklus geshuffel.")
+        print("3. Gaan uit.")
+        first_input = input()
+        strategy = 1
+
+        if first_input == "1" or first_input == "":
+            strategy = 1
+        elif first_input == "2":
+            strategy = 2
+        elif first_input == "3":
+            break
+        else:
+            print("Ongeldige opsie.")
+            break
+
+        print("Hoeveelheid maande? (default: 6)")
+        second_input = input()
+
+        if second_input == "":
+            months = 6
+        elif second_input.isdigit():
+            months = int(second_input)
+        else:
+            print("Ongeldige opsie.")
+            break
+
+        if strategy == 1:
+            generate_schedule(months)
+            break
+        if strategy == 2:
+            generate_schedule(months, True)
+            break
+        else:
+            print("Ongeldige opsie.")
+            break
+
+# Call the main function to start the program
+main()
---
+++ helper.py
@@ -0,0 +1,79 @@
+import csv
+import reportlab
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer, Paragraph
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+
+
+def read_diakens():
+    """
+    Reads the lines from the 'diakens.txt' file and returns them as a list.
+
+    Returns:
+        list: A list of lines from the 'diakens.txt' file.
+    """
+    with open('diakens.txt', 'r') as file:
+        lines = file.readlines()
+    return lines
+
+def output_csv(sundays, file_name):
+    with open(file_name, 'w', newline='') as file:
+        fieldnames = ['sondag_datum', 'diaken_1', 'diaken_2', 'diaken_3', 'diaken_4']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(sundays)
+
+def output_pdf(sundays, file_name):
+    """
+    Args:
+        sundays (list): list of dicts { sondag_datum: str, diaken_1: str, diaken_2: str, diaken_3: str, diaken_4: str }
+        file_name (str): file name for the pdf file
+    """
+    doc = SimpleDocTemplate(file_name, pagesize=A4)
+    elements = []
+
+    styles = getSampleStyleSheet()
+    # styleN = styles['Normal']
+    styleH1 = styles['Heading1']
+    styleH2 = styles['Heading2']
+    elements.append(Paragraph('DIENSBEURTE VIR DIAKENS', styleH1))
+
+    table_header = [['Datum', 'Punt 1', 'Punt 2', 'Punt 3', 'Punt 4']]
+
+    # Create table style
+    styleT = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+        ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 1), (-1, -1), 10),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ])
+
+    sundays_groups = [sundays[i:i + 4] for i in range(0, len(sundays), 4)]
+
+    print(sundays_groups)
+
+    for group in sundays_groups:
+        table_data = []
+        table_data.append(table_header)
+
+        for item in group:
+            table_data.append([item['sondag_datum'], item['diaken_1'], item['diaken_2'], item['diaken_3'], item['diaken_4']])
+
+        table = Table(table_data)
+        table.setStyle(styleT)
+
+        elements.append(Paragraph('Maart 2024', styleH2))
+        elements.append(table)
+
+    # Build the PDF document
+    doc.build(elements)
