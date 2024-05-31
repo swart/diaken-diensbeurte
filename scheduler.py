f331e5290824131e8388ff62a4be3873c004c579
pswart <paul.oorkant@gmail.com>
2024-05-31 21:22:25 +0200

    Changing scheduler to accommodate groups of names in the to-be-assigned list of diakens by assigning all diakens of a group to the same shift

--- scheduler.py
+++ scheduler.py
@@ -10,7 +10,8 @@ from helpers import (
     format_date_file_name,
     INPUT_FILE,
     OUTPUT_FILE_PREFIX,
-    OUTPUT_DIRECTORY
+    OUTPUT_DIRECTORY,
+    SHIFT_SIZE
 )
 from dateutil.relativedelta import relativedelta, SU
 import math
@@ -47,11 +48,68 @@ class Scheduler:
     def output_file_name(first_sunday, last_sunday, format):
         return f"{OUTPUT_FILE_PREFIX}_{format_date_file_name(first_sunday)}_tot_{format_date_file_name(last_sunday)}.{format.value}"
 
-    def generate(self):
+    def reserve_for_duty(self, to_be_assigned_list):
+        """
+        Return the names to be assigned to next shift and
+        remove them from the to-be-assigned list of names.
+
+        Args:
+            to_be_assigned_list : list of strings
+
+        Raises:
+            NotEnoughForShiftError
+
+        Returns:
+            on_duty_list : list of strings
+        """
+        on_duty_list = []
+        temp = {}
+
+        for i in range(SHIFT_SIZE):
+            # Split to also catch grouped names
+            entry = to_be_assigned_list[i].split(",")
+            # First add grouped names
+            for name in entry:
+                if len(entry) > 1:
+                    temp[name] = to_be_assigned_list[i]
+            # Now add individuals
+            for name in entry:
+                if len(entry) == 1:
+                    temp[name] = to_be_assigned_list[i]
+
+        # Raise exception if not enough for a shift
+        if len(temp) < SHIFT_SIZE:
+            raise NotEnoughForShiftError
+
+        # Set on duty list, constrain to shift size
+        on_duty_list = list(temp.keys())[0:SHIFT_SIZE]
+
+        # Remove from to be assigned list entries that we
+        # have names for in the on duty list. Constrain to
+        # Shift size. Expect duplicate values so convert it
+        # to a set.
+        entries_to_remove = set(list(temp.values())[0:SHIFT_SIZE])
+        for entry in set(temp.values()):
+            to_be_assigned_list.remove(entry)
+
+        return on_duty_list
+
+    def get_names(self):
+        """
+        Return read names in order determined by strategy
+
+        Return:
+            names: list of strings
+        """
         names = self.read_names()
 
         if self.strategy != Strategy.ORDERED:
             random.shuffle(names)
+
+        return names
+
+    def generate(self):
+        names = []
 
         current_month = datetime.now().month
         current_year = datetime.now().year
@@ -61,25 +119,24 @@ class Scheduler:
         last_sunday = first_day + relativedelta(months=self.months, weekday=SU(-1))
         end_date = first_day + relativedelta(months=self.months)
         weeks = math.ceil((end_date - first_sunday).days / 7)
-        
+
         sundays = []
         for week in range(weeks):
             sunday_datetime = first_sunday + timedelta(weeks=week)
-            names_starting_index = week * 4
 
-            sunday_diakens = []
-            for i in range(4):
-                diaken_index = (names_starting_index + i) % len(names)
-                diaken_name = names[diaken_index].strip()
-                sunday_diakens.append(diaken_name)
+            # If not enough unassigned names add some
+            if len(names) < SHIFT_SIZE:
+                names = names + self.get_names()
 
-            random.shuffle(sunday_diakens)
+            sunday_shift = self.reserve_for_duty(names)
+
+            random.shuffle(sunday_shift)
             sunday_schedule = {
                 "sondag_datum": sunday_datetime.date(),
-                "diaken_1": sunday_diakens[0],
-                "diaken_2": sunday_diakens[1],
-                "diaken_3": sunday_diakens[2],
-                "diaken_4": sunday_diakens[3],
+                "diaken_1": sunday_shift[0],
+                "diaken_2": sunday_shift[1],
+                "diaken_3": sunday_shift[2],
+                "diaken_4": sunday_shift[3],
             }
             sundays.append(sunday_schedule)
 
