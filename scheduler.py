5b295dab132682251d7735831932b6f74e4e352f
Ian Swart <ianswart13@gmail.com>
2024-06-07 20:41:17 +0200

    Cleanup

--- scheduler.py
+++ scheduler.py
@@ -12,22 +12,22 @@ from helpers import (
     OUTPUT_FILE_PREFIX,
     OUTPUT_DIRECTORY,
     SHIFT_SIZE,
-    SHIFT_SIZE
 )
 from dateutil.relativedelta import relativedelta, SU
 import math
 
+
 class Scheduler:
     def __init__(self, months=4, strategy=Strategy.RANDOM, include_current_month=False):
+        self.names = []
+        self.available_names = []
         self.months = months
         self.strategy = strategy
         self.include_current_month = include_current_month
 
     def read_names(self):
         with open(INPUT_FILE, "r") as file:
-            lines = file.readlines()
-
-        return lines
+            self.names = file.read().splitlines()
 
     def output_csv(self, sundays, file_name):
         with open(f"{OUTPUT_DIRECTORY}/{file_name}", "w", newline="") as file:
@@ -50,73 +50,47 @@ class Scheduler:
     def output_file_name(first_sunday, last_sunday, format):
         return f"{OUTPUT_FILE_PREFIX}_{format_date_file_name(first_sunday)}_tot_{format_date_file_name(last_sunday)}.{format.value}"
 
-    def reserve_for_duty(self, to_be_assigned_list):
+    def reserve_for_duty(self):
         """
         Return the names to be assigned to next shift and
         remove them from the to-be-assigned list of names.
 
-        Args:
-            to_be_assigned_list : list of strings
-
-        Raises:
-            NotEnoughForShiftError
-
         Returns:
             on_duty_list : list of strings
         """
+        # Make sure theres enough names to assign (available_names)
+        if len(self.available_names) < SHIFT_SIZE:
+            names = self.names.copy()
+
+            if self.strategy != Strategy.ORDERED:
+                random.shuffle(names)
+
+            self.available_names.extend(names)
+
+        # Assign four names to the next shift
         on_duty_list = []
-        temp = {}
-
         for i in range(SHIFT_SIZE):
-            # Split to also catch grouped names
-            entry = to_be_assigned_list[i].split(",")
-            # First add grouped names
-            for name in entry:
-                if len(entry) > 1:
-                    temp[name.rstrip()] = to_be_assigned_list[i]
-            # Now add individuals
-            for name in entry:
-                if len(entry) == 1:
-                    temp[name.rstrip()] = to_be_assigned_list[i]
+            item = self.available_names.pop(0)
 
-        # Raise exception if not enough for a shift
-        if len(temp) < SHIFT_SIZE:
-            raise NotEnoughForShiftError
+            # Groups of names that should be assigned together
+            on_duty_list.extend(item.split(","))
 
-        # Set on duty list, constrain to shift size
-        on_duty_list = list(temp.keys())[0:SHIFT_SIZE]
+            if len(on_duty_list) == SHIFT_SIZE:
+                break
 
-        # Remove from to be assigned list entries that we
-        # have names for in the on duty list. Constrain to
-        # Shift size. Expect duplicate values so convert it
-        # to a set.
-        entries_to_remove = set(list(temp.values())[0:SHIFT_SIZE])
-        for entry in set(temp.values()):
-            to_be_assigned_list.remove(entry)
+        for name in on_duty_list:
+            name = name.rstrip()
 
         return on_duty_list
 
-    def get_names(self, shuffle):
-        """
-        Return read names, optionally shuffled
-
-        Args:
-            shuffle : Place read names in random order
-
-        Return:
-            names: list of strings
-        """
-        names = self.read_names()
-
-        if shuffle == True:
-            random.shuffle(names)
-
-        return names
-
     def generate(self):
-        names = self.get_names((self.strategy != Strategy.ORDERED))
+        self.read_names()
 
-        current_month = datetime.now().month if self.include_current_month else datetime.now().month + 1
+        current_month = (
+            datetime.now().month
+            if self.include_current_month
+            else datetime.now().month + 1
+        )
         current_year = datetime.now().year
 
         first_day = datetime(current_year, current_month, 1)
@@ -128,14 +102,10 @@ class Scheduler:
         sundays = []
         for week in range(weeks):
             sunday_datetime = first_sunday + timedelta(weeks=week)
-
-            # If not enough unassigned names add some
-            if len(names) < SHIFT_SIZE:
-                names = names + self.get_names((self.strategy == Strategy.RANDOM_CYCLE))
-
-            sunday_shift = self.reserve_for_duty(names)
+            sunday_shift = self.reserve_for_duty()
 
             random.shuffle(sunday_shift)
+
             sunday_schedule = {
                 "sondag_datum": sunday_datetime.date(),
                 "diaken_1": sunday_shift[0],
